ee04ca82f84b9046a1544215c2fd9a7f12d7781c
BartoAlheiros <alheirosb@gmail.com>
2024-02-23 16:48:23 -0300

    exibindo lista de pokemons na home

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,35 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 
-const inter = Inter({ subsets: ["latin"] });
+export async function getStaticProps() {
 
-export default function Home() {
+  const maxPokemons = 251
+  const api = 'https://pokeapi.co/api/v2/pokemon/'
+
+  const res = await fetch(`${api}/?limit=${maxPokemons}`)
+  const data = await res.json()
+  
+  // add pokemon index
+  data.results.forEach((item, index) => {
+   item.id = index + 1 
+  })
+
+  return {
+    props: {
+      pokemons: data.results,
+    },
+  }
+
+}
+
+export default function Home({ pokemons }) {
   return (
-    <>
+    <div>
       <h1>PokeNext</h1>
-    </>
+      <ul>
+        {pokemons.map((pokemon) => (
+          <li key={pokemon.id}>{pokemon.name}</li>
+        ))}
+      </ul>
+    </div>
   );
 }
