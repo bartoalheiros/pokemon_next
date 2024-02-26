707fbd46eed56f359b0b261d39d8f31b296f6918
BartoAlheiros <alheirosb@gmail.com>
2024-02-26 16:42:15 -0300

    detalhes pokemons concluido

--- pages/pokemon/[pokemonId].js
+++ pages/pokemon/[pokemonId].js
@@ -1,3 +1,7 @@
+import Image from "next/image"
+
+import styles from '@/styles/Pokemon.module.css'
+
 export const getStaticPaths = async() => {
 
   const maxPokemons = 253
@@ -29,20 +33,43 @@ export const getStaticProps = async(context) => {
 
   let ret = [];
   
-  // add pokemon index
-  /*data.results.forEach((item, index) => {
-   if(item.name !== "unown") {
-    item.id = index + 1 
-    ret.push(item)
-   }
-  })*/
-
   return {
     props: { pokemon: data },
   }
 }
 
 export default function Pokemon({ pokemon }) {
-  console.log(pokemon)
-  return <p>{ pokemon.name }</p>
+  return (
+    <div className={styles.pokemon_container}>
+      <h1 className={styles.title}>{pokemon.name}</h1>
+      <Image 
+        src={`https://img.pokemondb.net/sprites/brilliant-diamond-shining-pearl/normal/${pokemon.name}.png`}
+        width="200"
+        height="200"
+        alt={pokemon.name}
+      />
+      <div>
+        <h3>Número:</h3>
+        <p>#{pokemon.id}</p>
+      </div>
+      <div>
+        <h3>Tipo:</h3>
+        <div className={styles.types_container}>
+          {pokemon.types.map((item, index) => (
+            <span key={index} className={`${styles.type} ${styles['type_' + item.type.name]}`}>{item.type.name}</span>
+          ))}
+        </div>
+      </div>
+      <div className={styles.data_container}>
+        <div className={styles.data_height}>
+          <h3>Altura:</h3>
+          <p>{pokemon.height * 10} cm</p>
+        </div>
+        <div className={styles.data_weight}>
+          <h3>Peso:</h3>
+          <p>{pokemon.weight / 10} kg</p>
+        </div>
+      </div>
+    </div>
+  )
 }
